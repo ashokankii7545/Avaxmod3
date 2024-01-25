0e8b063bce129927ccc6037b7bbdbd5446327777
ashokankii7545 <155290845+ashokankii7545@users.noreply.github.com>
2024-01-25 10:36:12 +0530

    Update mock.sol

--- mock.sol
+++ mock.sol
@@ -1,1 +1,20 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+
+contract UniqueToken is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("UniqueToken", "UTK")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 1 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
